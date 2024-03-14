fb2f3ea3f8ec28bd17c129a27b9acc524ccbe9bf
Thom <22082476@student.hhs.nl>
2024-03-14 11:16:49 +0100

    next commit

--- mechanical-portal/src/Typer.js
+++ mechanical-portal/src/Typer.js
@@ -1,47 +1,72 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Typer.css'
+import json from './words.json'
 
-export function Typer () 
-{
-    const [text, setText] = useState(["kaas", "aap", "hoofd", "banaan", "appel", "wortel"]);
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [incorrect, setIncorrect] = useState([])
+export function Typer() {
+    const [text, setText] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [incorrect, setIncorrect] = useState([]);
 
-    const handleSpaceBar = (e) => 
+    useEffect(()=>
     {
-        if (e.keyCode == 32)
-        {
+        setWords(10) 
+    }, [])
+    
+    
+    const handleSpaceBar = (e) => {
+        if (e.keyCode === 32) {
+            const input = e.target.value.trim();
             e.preventDefault();
 
-            checkInput(e.target.value);
+            checkInput(input);
 
             document.getElementById("input-field").value = "";
-
 
             setCurrentIndex(currentIndex + 1);
         }
     };
 
-    const checkInput = (input) =>
-    {
-        if (input != text[currentIndex])
-        {
-            setIncorrect([...incorrect, text[currentIndex]])
-        }       
-    }; 
+    const checkInput = (input) => {
+        if (input !== text[currentIndex]) {
+            setIncorrect([...incorrect, currentIndex]);
+        }
+    };
+
+    const setWords = (count) => {
+        const newWords = [];
+        for (let i = 0; i < count; i++) {
+            const randomIndex = Math.floor(Math.random() * json.words.length);
+            newWords.push(json.words[randomIndex]);
+        }
+        setText(newWords);
+        setCurrentIndex(0);
+        setIncorrect([]);
+    };
 
     return (
         <div className="TypeDiv">
-            <div>
-            <div>
-                {text && 
-                    text.map((word, index) => 
-                    (
-                        <span className={index == currentIndex ? "active" :  index < currentIndex ? (text[index] in incorrect ? "incorrect" : "correct") :  "not-active"}key={index}>{word} </span>
-                    )
-                )}
-            </div>
-            <input id="input-field" type="text" onKeyUp={handleSpaceBar}></input>
+            <div className="center-div">
+                <span>
+                    <button className={text.length == 10 ? "button-active" : "button"} onClick={() => setWords(10)}>10</button>
+                    /
+                    <button className={text.length == 25 ? "button-active" : "button"} onClick={() => setWords(25)}>25</button>
+                    /
+                    <button className={text.length == 50 ? "button-active" : "button"} onClick={() => setWords(50)}>50</button>
+                    /
+                    <button className={text.length == 100 ? "button-active" : "button"} onClick={() => setWords(100)}>100</button>
+                    /
+                    <button className={text.length == 250 ? "button-active" : "button"} onClick={() => setWords(250)}>250</button>
+                </span>
+                <div>
+                    {text.map((word, index) => (
+                        <span className={index === currentIndex ? "active" : (index < currentIndex ? (incorrect.includes(index) ? "incorrect" : "correct") : "not-active")} key={index}>{word} </span>
+
+                    ))}
+                </div>
+                <span>
+                    <input id="input-field" type="text" onKeyUp={handleSpaceBar}></input>
+                    <button>redo</button>
+                </span>
             </div>
         </div>
     );
